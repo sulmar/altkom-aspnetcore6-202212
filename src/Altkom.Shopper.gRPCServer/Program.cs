21acb876b5517a39d3c7e61745f8c837160f018b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-15 11:00:59 +0100

    Dodanie refleksji

--- src/Altkom.Shopper.gRPCServer/Program.cs
+++ src/Altkom.Shopper.gRPCServer/Program.cs
@@ -8,13 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+// dotnet add package Grpc.AspNetCore.Server.Reflection
+builder.Services.AddGrpcReflection();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
+// grpcurl
+// https://github.com/fullstorydev/grpcurl/releases
+
+// grpcurl --plaintext -d {\"book_id\":1}  localhost:5164 Inventory/GetBook
 
 app.MapGrpcService<InventoryService>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 app.Run();
