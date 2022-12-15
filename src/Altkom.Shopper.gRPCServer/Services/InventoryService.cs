2f78bc7b4dd59d6f6b2b2c60c8de14b7ebc53d55
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-15 11:24:36 +0100

    Pobieranie listy obiektów

--- src/Altkom.Shopper.gRPCServer/Services/InventoryService.cs
+++ src/Altkom.Shopper.gRPCServer/Services/InventoryService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bookshop;
+using Grpc.Core;
 
 namespace Altkom.Shopper.gRPCServer.Services
 {
@@ -28,6 +29,41 @@ namespace Altkom.Shopper.gRPCServer.Services
             };
             
             return Task.FromResult(book);
+        }
+
+        public override Task<GetBooksResponse> GetBooks(GetBooksRequest request, ServerCallContext context)
+        {
+            var books = new List<Book>
+            {
+                new Book { 
+                    Id = 1,
+                    Title = "ASP.NET Core 6",
+                    Author = "John Smith",
+                    PageCount = 500,
+                    Price = 199d,
+                },
+
+                new Book { 
+                    Id = 2,
+                    Title = "gRPC in Action",
+                    Author = "John Smith",
+                    PageCount = 250,
+                    Price = 99d,
+                },
+
+                  new Book { 
+                    Id = 3,
+                    Title = "REST API in Action",
+                    Author = "Bob Smith",
+                    PageCount = 450,
+                    Price = 119d,
+                },
+            };
+
+            var response = new GetBooksResponse();
+            response.Books.AddRange(books);
+
+            return Task.FromResult(response);
         }
     }
         
